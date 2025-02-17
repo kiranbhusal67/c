ab2d1f480a57f95c01269af70722b8e51b359fd3
kiranbhusal67 <kiranbhusal67@gmail.com>
2025-02-17 17:12:35 +0545

    Create 5th.c

---
+++ 5th.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main () {
+    int a,b,product;
+    printf("Enter any two numbers");
+    scanf("%d %d",&a,&b);
+    product = a*b;
+    printf("THE Product is%d",product);
+    getch ();
+    return 0;
+}
