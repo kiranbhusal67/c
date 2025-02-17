7505a7010afce9d72cafb3b70c0a695cd4a58e14
kiranbhusal67 <kiranbhusal67@gmail.com>
2025-02-17 17:13:35 +0545

    Create 7th.c

---
+++ 7th.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+int i,j;
+int mat1[3][2],mat2[3][2],sum[3][2];
+printf("Enter elements of first matrix:\n ");
+
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<2;j++)
+        {
+            printf("mat1[%d][%d]= ",i,j);
+            scanf("%d",&mat1[i][j]);
+        }
+        }
+
+        printf("Enter elements of second matrix:\n ");
+
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<2;j++)
+        {
+            printf("mat2[%d][%d]= ",i,j);
+            scanf("%d",&mat2[i][j]);
+        }
+}
+printf("The sum of the matrix is:\n");
+        for(i=0;i<3;i++)
+        {
+            for(j=0;j<2;j++)
+            {
+                sum[i][j]=mat1[i][j]+mat2[i][j];
+                printf("%d\t",sum[i][j]);
+            }
+            printf("\n");
+        }
+        return 0;
+}
+
