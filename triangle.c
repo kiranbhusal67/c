7d7311113b7e2da7f08c2c7c269ec85f330da215
kiranbhusal67 <kiranbhusal67@gmail.com>
2025-02-25 12:53:44 +0545

    Create triangle.c

---
+++ triangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+main()
+{
+	int b,c,h,s,a;
+	printf("Enter the sides of triangle");
+	scanf("%d %d %d ",&b,&h,&c);
+s=(b+c+h)/2;
+	a=sqrt(s*(s-h)*(s-b)*(s-c));
+	printf("the area of triang;e %d ",a);
+	
+}
